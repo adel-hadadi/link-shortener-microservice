5ddf01faab01b240a66d5a2fa8128069dfd8cc75
Adel hadadi <hadadiadel81@gmail.com>
2024-11-09 11:12:31 +0330

    fix: append to report excel file

--- internal/report/ports/scheduler.go
+++ internal/report/ports/scheduler.go
@@ -2,8 +2,10 @@ package ports
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/xuri/excelize/v2"
@@ -14,6 +16,7 @@ import (
 
 const (
 	ErrNewScheduler = "error on get new scheduler: %w"
+	reportSheet     = "Clicks Report"
 )
 
 func RunScheduler(app app.Application) {
@@ -55,43 +58,79 @@ func generateExcelReport(app app.Application) {
 		log.Fatal(err)
 	}
 
-	log.Println("clicks count => ", len(clicks))
+	now := time.Now().Format("2006-01-02")
+	filePath := fmt.Sprintf("./storage/clicks-report-%s.xlsx", now)
 
-	f := excelize.NewFile()
+	f, err := getReportFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	defer func() {
-		if err := f.Close(); err != nil {
+		if err := f.SaveAs(filePath); err != nil {
 			log.Fatal(err)
 		}
+		f.Close()
 	}()
 
-	sheet := "Clicks Report"
-	index, err := f.NewSheet(sheet)
+	rows, err := f.GetRows(reportSheet)
 	if err != nil {
 		log.Println(err)
+		return
 	}
 
-	headers := []string{"Short URL"}
-	for hour := 0; hour < 24; hour++ {
-		headers = append(headers, fmt.Sprintf("%02d:00", hour))
+	currentHour := time.Now().Hour()
+
+	for _, click := range clicks {
+		if index, exists := checkShortURLExists(click.ShortURL, rows); exists {
+			cell, _ := excelize.CoordinatesToCellName(currentHour+2, index+1)
+			f.SetCellValue(reportSheet, cell, click.Count)
+			continue
+		}
+
+		cell, _ := excelize.CoordinatesToCellName(1, len(rows)+1)
+		f.SetCellValue(reportSheet, cell, click.ShortURL)
+
+		countCell, _ := excelize.CoordinatesToCellName(currentHour+2, len(rows)+1)
+		f.SetCellValue(reportSheet, countCell, click.Count)
+	}
+}
+
+func getReportFile(filePath string) (*excelize.File, error) {
+	if _, err := os.Stat(filePath); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			f := excelize.NewFile()
+
+			sheetIndex, err := f.NewSheet(reportSheet)
+			if err != nil {
+				log.Println(err)
+			}
+
+			headers := []string{"Short URL"}
+			for hour := 0; hour < 24; hour++ {
+				headers = append(headers, fmt.Sprintf("%02d:00", hour))
+			}
+
+			for i, header := range headers {
+				cell, _ := excelize.CoordinatesToCellName(i+1, 1)
+				f.SetCellValue(reportSheet, cell, header)
+			}
+
+			f.SetActiveSheet(sheetIndex)
+
+			return f, nil
+		}
 	}
 
-	for i, header := range headers {
-		cell, _ := excelize.CoordinatesToCellName(i+1, 1)
-		f.SetCellValue(sheet, cell, header)
+	return excelize.OpenFile(filePath)
+}
+
+func checkShortURLExists(shortURL string, rows [][]string) (int, bool) {
+	for i, row := range rows {
+		if row[0] == shortURL {
+			return i, true
+		}
 	}
 
-	for i, click := range clicks {
-		cell, _ := excelize.CoordinatesToCellName(1, i+2)
-		f.SetCellValue(sheet, cell, click.ShortURL)
-
-		countCell, _ := excelize.CoordinatesToCellName(time.Now().Hour()+2, i+2)
-
-		f.SetCellValue(sheet, countCell, click.Count)
-	}
-
-	f.SetActiveSheet(index)
-
-	if err := f.SaveAs("storage/report.xlsx"); err != nil {
-		log.Fatal(err)
-	}
+	return 0, false
 }
