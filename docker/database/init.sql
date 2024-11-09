aa4410f1508d3a3c6bb21625a88a6eff3d430964
Adel hadadi <hadadiadel81@gmail.com>
2024-11-09 18:28:22 +0330

    refactor: remove additionals

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -9,11 +9,3 @@ CREATE TABLE IF NOT EXISTS links
 );
 
 CREATE UNIQUE INDEX link_idx ON links (original_link);
-
-create table if not exists clicks
-(
-    id         bigserial primary key,
-    link_id    serial references links,
-    ip_address varchar(45) not null,
-    created_at timestamp default now()
-)
